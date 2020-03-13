3a98b42fef50e6367ec47f6400ad27219f477e31
Olle Fredriksson <fredriksson.olle@gmail.com>
2020-03-13 18:16:24 +0100

    wip

--- examples/User.hs
+++ examples/User.hs
@@ -16,6 +16,8 @@ import qualified Generics.SOP as SOP
 
 import qualified Language.Elm.Pretty as Pretty
 import qualified Language.Elm.Simplification as Simplification
+import qualified Language.Elm.Type as Type
+import qualified Language.Elm.Expression as Expression
 import Language.Haskell.To.Elm
 
 data User a b = User
@@ -29,21 +31,32 @@ instance HasElmType User where
   elmDefinition =
     Just $ deriveElmTypeDefinition @User defaultOptions "Api.User.User"
 
--- instance HasElmDecoder Aeson.Value a => HasElmDecoder Aeson.Value (User a) where
---   elmDecoderDefinition =
---     Just $ deriveElmJSONDecoder @User defaultOptions Aeson.defaultOptions "Api.User.decoder"
+instance (HasElmType a, HasElmType b) => HasElmType (User a b) where
+  elmType =
+    Type.apps (elmType @User) [elmType @a, elmType @b]
 
--- instance HasElmEncoder Aeson.Value a => HasElmEncoder Aeson.Value (User a) where
---   elmEncoderDefinition =
---     Just $ deriveElmJSONEncoder @User defaultOptions Aeson.defaultOptions "Api.User.encoder"
+instance HasElmDecoder Aeson.Value User where
+  elmDecoderDefinition =
+    Just $ deriveElmJSONDecoder @User defaultOptions Aeson.defaultOptions "Api.User.decoder"
+
+instance HasElmEncoder Aeson.Value User where
+  elmEncoderDefinition =
+    Just $ deriveElmJSONEncoder @User defaultOptions Aeson.defaultOptions "Api.User.encoder"
+
+instance (HasElmDecoder Aeson.Value a, HasElmDecoder Aeson.Value b) => HasElmDecoder Aeson.Value (User a b) where
+  elmDecoder =
+    Expression.apps (elmDecoder @Aeson.Value @User) [elmDecoder @Aeson.Value @a, elmDecoder @Aeson.Value @b]
+
+instance (HasElmEncoder Aeson.Value a, HasElmEncoder Aeson.Value b) => HasElmEncoder Aeson.Value (User a b) where
+  elmEncoder =
+    Expression.apps (elmEncoder @Aeson.Value @User) [elmEncoder @Aeson.Value @a, elmEncoder @Aeson.Value @b]
 
 main :: IO ()
 main = do
   let
     definitions =
       Simplification.simplifyDefinition <$>
-        -- jsonDefinitions @(User Int)
-        catMaybes [elmDefinition @User]
+        jsonDefinitions @User
 
     modules =
       Pretty.modules definitions
